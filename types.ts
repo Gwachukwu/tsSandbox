4481d1eaf1432eba03d26bbeb35daace0e20bdf3
Gwachukwu Elebeke <55688650+Gwachukwu@users.noreply.github.com>
2020-12-09 15:17:40 +0100

    first commit

---
+++ types.ts
@@ -0,0 +1,32 @@
+export {};
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// let strArr: string[];
+// let numArr: number[];
+// let boolArr: boolean[];
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string,number];
+
+myString = "Hello world";
+myNum = 22;
+myBool = true;
+myVar = "Anything";
+boolArr = [true, false];
+
+strArr = ["Hello", "World"];
+numArr = [1, 2, 3];
+
+strNumTuple=["hello",7]
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = null;
+
+console.log(strArr);
